cc883e4d70e1095171ceae95f7c618e57041f00d
Uema Yohei <winuim@gmail.com>
2020-05-02 23:55:57 +0900

    crashlytics更新

--- lib/demo.dart
+++ lib/demo.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DemoPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-import 'app.dart';
-// import 'firebase_ml_vision/example.dart';
+// import 'app.dart';
+import 'firebase_ml_vision/example.dart';
 // import 'barcode_scanner/example.dart';
 
-Future<void> main() async {
+void main() {
   // Set `enableInDevMode` to true to see reports while in debug mode
   // This is only to be used for confirming that reports are being
   // submitted as expected. It is not intended to be used for everyday
@@ -18,7 +18,7 @@ Future<void> main() async {
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runZoned(() {
-      // Fetch the available cameras before initializing the app.
+    // Fetch the available cameras before initializing the app.
     runApp(MyApp());
   }, onError: Crashlytics.instance.recordError);
 }
